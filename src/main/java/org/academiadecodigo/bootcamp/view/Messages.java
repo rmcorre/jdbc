cd4ab586c4e2c975556b59fb64669f2a78ec39b0
Horacio Correia <rmcorre@gmail.com>
2020-03-06 17:10:00 -0100

    Deleted db.sql Modify App: Edit dbUrl for ConnectionManager, Create userTable Modify Messages: Ediit STARTUP_MSG

--- src/main/java/org/academiadecodigo/bootcamp/view/Messages.java
+++ src/main/java/org/academiadecodigo/bootcamp/view/Messages.java
@@ -2,7 +2,7 @@ package org.academiadecodigo.bootcamp.view;
 
 public class Messages {
 
-    public final static String STARTUP_MSG = "Welcome to your first Prompt-View Experience!";
+    public final static String STARTUP_MSG = "Welcome to the User Database!";
 
     public final static String LOGIN_MSG = "Insert your login credentials below:";
     public static final String LOGIN_ERROR = "Username and/or password incorrect.";
